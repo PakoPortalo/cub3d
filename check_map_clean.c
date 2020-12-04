5cd9ed4c5ba5daaec7213ea4d6e077be55361a62
PakoPortalo <PakoPortalo@users.noreply.github.com>
2020-12-04 10:22:18 +0100

    Cleaning map 4 - Map Check 2

--- check_map_clean.c
+++ check_map_clean.c
@@ -6,7 +6,7 @@
 /*   By: fportalo <fportalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 10:18:04 by fportalo          #+#    #+#             */
-/*   Updated: 2020/12/03 11:23:28 by fportalo         ###   ########.fr       */
+/*   Updated: 2020/12/04 09:50:17 by fportalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,47 +66,40 @@ int	find_origin(mapstr *raw)
 	return (1);
 }
 
-int		check_border(mapstr *raw)
+int		check_border(mapstr *raw, int y, int x)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (i < raw->rows) //lineas
+	if (y == 0 || y == (raw->rows - 1) || x == 0  || x == ((int)ft_strlen(raw->map[y]) - 1))
 	{
-		j = 0;
-		while (j < (int)ft_strlen(raw->map[i]))
-		{
-			if (raw->map[i][0] != '1' || raw->map[0][j] != '1')
-			{
-				printf("Error. Your map must be completly enclosed by walls");
-				return (-1);
-			}
-			if (raw->map[i][ft_strlen(raw->map[i]) - 1] != '1' || raw->map[raw->rows - 1][j] != '1')
-			{
-				printf("Error. Your map must be completly enclosed by walls");
-				return (-1);
-			}
-			j++;
-		}
-		i++;
+		printf("Opened in %dx, %dy\n", x, y);
+		printf("ERROR. You need to introduce a correct map\n");
+		return (-1);
 	}
 	return (1);
 }
 
 int		flood_fill(mapstr *raw, int y, int x)
 {
-	if (ft_strchr("NSWE0", raw->map[y][x]))
+	if (check_border(raw, y, x) == -1)
+		return (-1);
+	if (ft_strchr("NSWE0 ", raw->map[y][x]))
 		raw->map[y][x] = '3';
 
-	if(raw->map[y - 1][x] == '0')
+	if(raw->map[y - 1][x] == '0' || raw->map[y - 1][x] == ' ')
 		flood_fill(raw, y - 1, x);
-	if(raw->map[y][x + 1] == '0')
+	if(raw->map[y][x + 1] == '0' || raw->map[y][x + 1] == ' ')
 		flood_fill(raw, y, x + 1);
-	if(raw->map[y + 1][x] == '0')
+	if(raw->map[y + 1][x] == '0' || raw->map[y + 1][x] == ' ')
 		flood_fill(raw, y + 1, x);
-	if(raw->map[y][x - 1] == '0')
+	if(raw->map[y][x - 1] == '0' || raw->map[y][x - 1] == ' ')
 		flood_fill(raw, y, x - 1);
+	// if(raw->map[y + 1][x + 1] == '0' || raw->map[y + 1][x + 1] == ' ')
+	// 	flood_fill(raw, y + 1, x + 1);
+	// if(raw->map[y - 1][x + 1] == '0' || raw->map[y - 1][x + 1] == ' ')
+	// 	flood_fill(raw, y - 1, x + 1);
+	// if(raw->map[y - 1][x - 1] == '0' || raw->map[y - 1][x - 1] == ' ')
+	// 	flood_fill(raw, y - 1, x - 1);
+	// if(raw->map[y + 1][x - 1] == '0' || raw->map[y + 1][x - 1] == ' ')
+	//	flood_fill(raw, y + 1, x - 1);
 
 	if (raw->map[y][x] == '\0')
 	{
@@ -122,8 +115,8 @@ int		check_map(mapstr *raw, mapclean *map)
 	map = NULL;
 	if (find_origin(raw) == -1)
 		return (-1);
-	if (check_border(raw) == -1)
-		return (-1);
+	// if (check_border(raw) == -1)
+	// 	return (-1);
 	if (flood_fill(raw, raw->y , raw->x) == -1)
 		return (-1);
 	return (1);
